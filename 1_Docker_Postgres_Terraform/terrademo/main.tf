51a4d28837cd31bf1c01061b21f99749556c5738
Hasan John Kideys <hasanjohnkideys@Hasans-MacBook-Air.local>
2026-01-17 18:16:05 +0000

    terraform files_1

--- 1_Docker_Postgres_Terraform/terrademo/main.tf
+++ 1_Docker_Postgres_Terraform/terrademo/main.tf
@@ -11,3 +11,20 @@ provider "google" {
   project = "terraform-demo-484521"
   region  = "us-central1"
 }
+
+
+
+resource "google_storage_bucket" "auto-expire" {
+  name          = "terraform-demo-484521-auto-expiring-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "Delete"
+    }
+  }
+}
